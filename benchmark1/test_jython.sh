2d1862f07189d95972226ebe473f737a03ad53aa
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-10 14:33:47 +0200

    Refactored scripts a bit

--- benchmark1/test_jython.sh
+++ benchmark1/test_jython.sh
@@ -1,7 +1,13 @@
 sizes="16 24 32 48 64 96 128 192 256 384 512 768 1024 1536 2048"
 
+OUTFILE="jython.times"
+PREFIX="jython"
+
+rm $OUTFILE
+
 for size in $sizes
 do
     echo $size
-    /usr/bin/time --output jython.times --append --format "%e %M" java -jar jython-standalone-2.7.0.jar mandelbrot.py $size $size 255 > "j_${size}_${size}.ppm"
+    echo -n "$size " >> $OUTFILE
+    /usr/bin/time --output $OUTFILE --append --format "%e %M" java -jar jython-standalone-2.7.0.jar mandelbrot.py $size $size 255 > "${PREFIX}_${size}_${size}.ppm"
 done
--- benchmark1/test_python3.sh
+++ benchmark1/test_python3.sh
@@ -1,7 +1,13 @@
 sizes="16 24 32 48 64 96 128 192 256 384 512 768 1024 1536 2048"
 
+OUTFILE="python3.times"
+PREFIX="python3"
+
+rm $OUTFILE
+
 for size in $sizes
 do
     echo $size
-    /usr/bin/time --output python3.times --append --format "%e %M" python3 mandelbrot.py $size $size 255 > "p3_${size}_${size}.ppm"
+    echo -n "$size " >> $OUTFILE
+    /usr/bin/time --output $OUTFILE --append --format "%e %M" python3 -B mandelbrot.py $size $size 255 > "${PREFIX}_${size}_${size}.ppm"
 done
