1b6688304ca340f61a94b9112aef3da5430cb12f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-07 09:06:12 +0200

    Calling getters from Color class

---
+++ color_getters/color_getters.py
@@ -0,0 +1,28 @@
+from java.awt import Color
+
+print("red")
+
+c = Color(1, 0, 0)
+
+print(c.getRed())
+print(c.getGreen())
+print(c.getBlue())
+print(c.getAlpha())
+
+print("")
+
+print(c.red)
+print(c.green)
+print(c.blue)
+print(c.alpha)
+
+
+print("\n\nyellow")
+
+c2 = Color.YELLOW
+
+print(c2.red)
+print(c2.green)
+print(c2.blue)
+print(c2.alpha)
+
