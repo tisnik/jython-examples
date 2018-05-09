10a1e9ac04db4ca9c7c40581c388f2032f63ac39
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-09 12:45:09 +0200

    Improvement version of the Sieve algorithm implementation

--- benchmark_sieve_algorithm/sieve_algorithm.py
+++ benchmark_sieve_algorithm/sieve_algorithm.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from sys import argv
+
+
 def sieve(n):
     multiples = set()
     for i in range(2, n+1):
         if i not in multiples:
             yield i
             multiples.update(range(i*i, n+1, i))
- 
-print(len(list(sieve(10000000))))
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("usage: python sieve_algorithm repeat_count")
+        exit(1)
+
+    max_value = int(argv[1])
+    primes = list(sieve(max_value))
+    print(len(primes))
